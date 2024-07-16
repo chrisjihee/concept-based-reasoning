29a1b9c4f81759d089c6cb4a3d303814ce151de8
Jihee Ryu <chrisjihee@etri.re.kr>
2024-07-17 00:11:15 +0900

    Update evaluation-by-GPT4.py

--- evaluation-by-GPT4.py
+++ evaluation-by-GPT4.py
@@ -1,58 +1,55 @@
 from getpass import getpass
 
-from openai import OpenAI
-from tqdm import tqdm
-
+from chrisbase.data import *
 from chrisbase.io import *
 
+logger = logging.getLogger(__name__)
+args = CommonArguments(
+    env=ProjectEnv(
+        project="LLM-based",
+        job_name="LLM-based-generation",
+        msg_level=logging.INFO,
+        msg_format=LoggingFormat.PRINT_00,
+    )
+)
+
 # setup arguments
-test_size = 1
-request_timeout = 300
+test_size = 3
 input_file = "data/LLM-test-with-KG-responses-3.json"
-prompt_template = read_or("template/extraction_prompt.txt") or getpass("Extraction Prompt: ")
-api_client = OpenAI(timeout=request_timeout,
-                    api_key=read_or("conf/key-openai.txt") or getpass("OpenAI API key: "))
+output_file = "data/LLM-test-with-KG-evaluation-prompt-{i}.txt"
+prompt_template = read_or("template/evaluation_prompt.txt") or getpass("Evaluation Prompt: ")
 
 
-# define function to chat with LLM
-def chat_with_gpt(messages, model_id="gpt-4o"):
-    try:
-        stream = api_client.chat.completions.create(
-            model=model_id,
-            messages=messages,
-            stream=True,
-        )
-        return ''.join(chunk.choices[0].delta.content or "" for chunk in stream)
-    except:
-        return None
+def limit_words(text, max_words):
+    text = text.replace("\n", "<BR> </BR>")
+    text = ' '.join(text.split(' ')[:max_words])
+    text = text.replace("<BR> </BR>", "\n")
+    return text
 
 
 # read input file
 input_data = load_json(input_file)
-input_data = input_data[:test_size]
-for i, item in enumerate(input_data, start=1):
-    chat_history = []
-    question = item["question"]
-    base_answer = item["answer"]
-    base_triples = "\n".join([f"  - {triple}" for triple in item["triples"]])
-    model_responses = []
-    for response in item["responses"]:
-        model = response["model"].split("/")[-1]
-        output = response["output"]
-        model_responses.append(f"\n----------\n[{model}]:\n{output}\n----------\n\n")
-    extraction_prompt = prompt_template.format(
-        question=question,
-        base_answer=base_answer,
-        base_triples=base_triples,
-        model_responses="\n".join(model_responses),
-        num_model=len(model_responses),
-    )
-    chat_history.append({"role": "user", "content": extraction_prompt})
-    print("=" * 120)
-    print(extraction_prompt)
-    print("=" * 120)
-    print("\n" * 5)
-    model_response = chat_with_gpt(model_id="gpt-4o", messages=chat_history)
-    print("=" * 120)
-    print(response)
-    print("=" * 120)
+
+# chat with GPT
+with JobTimer("Prompt Generation", rt=1, rb=1, rw=114, rc='=', verbose=1):
+    total_data = []
+    input_data = input_data[:test_size]
+    for i, item in enumerate(input_data, start=1):
+        total_data.append(item)
+        question = item["question"]
+        base_answer = item["answer"]
+        base_triples = "\n".join([f"  - {triple}" for triple in item["triples"]])
+        max_output_words = int(item["avg_words"] * 1.5)
+        model_responses = []
+        for response in item["responses"]:
+            model = response["model"].split("/")[-1]
+            output = limit_words(response["output"], max_words=max_output_words)
+            model_responses.append(f"\n<BEGIN_OF_MODEL_RESPONSE>\n[{model}]:\n{output}\n<END_OF_MODEL_RESPONSE>\n\n")
+        evaluation_prompt = prompt_template.format(
+            question=question,
+            base_answer=base_answer,
+            base_triples=base_triples,
+            model_responses="\n".join(model_responses),
+            num_model=len(model_responses),
+        )
+        write_or(output_file.format(i=i), evaluation_prompt)
