6b9cb7cb9e88e3512793e4b82b3b4a709064e220
Jihee Ryu <chrisjihee@etri.re.kr>
2024-07-16 19:15:22 +0900

    Update .gitignore

--- LLMs-evaluation.py
+++ LLMs-evaluation.py
@@ -1,22 +1,21 @@
 from getpass import getpass
 
-from together import Together
 from tqdm import tqdm
 
 from chrisbase.io import *
 
 # setup arguments
-input_file = "data/LLM-test-with-KG-responses.json"
+input_file = "data/LLM-test-with-KG-responses-3.json"
 output_file = "data/LLM-test-with-KG-extraction-prompt-{i}.txt"
 prompt_template = read_or("template/extraction_prompt.txt") or getpass("Enter the prompt template: ")
 
-total_responses = load_json(input_file)
-for i, qa_response in enumerate(total_responses, start=1):
-    question = qa_response["question"]
-    base_answer = qa_response["answer"]
-    base_triples = "\n".join([f"  - {triple}" for triple in qa_response["triples"]])
+total_data = load_json(input_file)
+for i, item in enumerate(tqdm(total_data, desc=f"* Making extraction prompt", unit="item"), start=1):
+    question = item["question"]
+    base_answer = item["answer"]
+    base_triples = "\n".join([f"  - {triple}" for triple in item["triples"]])
     model_responses = []
-    for response in qa_response["responses"]:
+    for response in item["responses"]:
         model = response["model"].split("/")[-1]
         output = response["output"]
         model_responses.append(f"\n----------\n[{model}]:\n{output}\n----------\n\n")
