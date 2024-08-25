7f8086fe994ac43cfdd62d7a56bfeed6aeef8ee2
Jihee Ryu <chrisjihee@etri.re.kr>
2024-08-26 03:28:14 +0900

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -4,13 +4,11 @@ wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforg
 bash Miniforge3-$(uname)-$(uname -m).sh
 
 # basic
-mamba create -n LLM-based python=3.11 -y
-mamba activate LLM-based
+mamba create -n LLM-based python=3.11 -y; mamba activate LLM-based
 pip install -r requirements.txt
 
 # chrisbase
-rm -rf chrisbase*
-git clone https://github.com/chrisjihee/chrisbase.git
+rm -rf chrisbase*; git clone git@github.com:chrisjihee/chrisbase.git
 pip install --editable chrisbase*
 
 # list
