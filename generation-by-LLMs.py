f836c53039ce2d02d2a6097355a168fc7123cb44
Jihee Ryu <chrisjihee@etri.re.kr>
2024-08-26 14:37:55 +0900

    Use output_triples_hint

--- generation-by-LLMs.py
+++ generation-by-LLMs.py
@@ -56,7 +56,6 @@ def chat_with_llm(messages, model_id,
 
 dataset = "WN18RR"
 input_file = f"data/{dataset}/edges_as_text_all.tsv"
-output_file = f"data/{dataset}/edges_as_text_all-responses-{test_size}.json"
 target_models = [x["full_id"] for x in load_json("conf/llama_chat_models.json")]
 prompt_template = read_or("template/generation_prompt.txt") or getpass("Generation Prompt: ")
 demo_template_match = re.search(r'<BEGIN_OF_DEMO_EXAMPLE>(?s:.)+<END_OF_DEMO_EXAMPLE>', prompt_template)
@@ -64,13 +63,13 @@ demo_template = prompt_template[demo_template_match.start(): demo_template_match
 
 all_samples = list(tsv_lines(input_file))
 relations = sorted({x[1] for x in all_samples})
-# relations = '\n'.join(f'- {a}' for a in sorted({x[1] for x in all_samples}))
 
 total_data = [{"entity": k, "triples": list(v)} for k, v in grouped(all_samples, key=lambda x: x[0])]
 train_data, test_data = train_test_split(total_data, test_size=test_size, random_state=7)
 print(f"- #relations: {len(relations)}")
 print(f"- #test entities: {len(test_data)}")
 print(f"- #train entities: {len(train_data)}")
+print(f"- #target_models: {len(target_models)}")
 
 test_data_per_size = {k: list(v) for k, v in grouped(test_data, key=lambda x: len(x["triples"]))}
 train_data_per_size = {k: list(v) for k, v in grouped(train_data, key=lambda x: len(x["triples"]))}
@@ -86,16 +85,19 @@ demo_prompts = [
     ) for demo in demo_data
 ]
 generation_prompt = prompt_template[:demo_template_match.start()].format(
-    relations='\n'.join(f'- {a}' for a in sorted({x[1] for x in all_samples}))
+    relations='\n'.join(f'- {a}' for a in relations)
 ) + "\n\n".join(demo_prompts) + prompt_template[demo_template_match.end():]
 generation_levels = {
-    1: "relation_only",
-    2: "tail_only",
+    1: "relation_only",  # Relation Classification
+    2: "tail_only",  # Link Prediction
     3: "tail_with_relation",
-    4: "free_form",
+    4: "free_with_quantity",
+    5: "free_without_quantity",
 }
 generation_level = 3
 print(f"- generation_level: {generation_level}")
+
+output_file = f"data/{dataset}/edges_as_text_all-responses-{test_size}@{generation_level}.json"
 
 # chat with LLMs
 with JobTimer("KG Generation", rt=1, rb=1, rw=114, rc='=', mt=1, verbose=1):
@@ -108,25 +110,31 @@ with JobTimer("KG Generation", rt=1, rb=1, rw=114, rc='=', mt=1, verbose=1):
         test_entity = item["entity"]
         test_triples = item["triples"]
         test_triples_size = len(test_triples)
+
         if generation_level == 1:
             output_triples = '\n'.join([f'  - {h} -> (predict relation here) -> {t}' for (h, r, t) in test_triples])
+            output_triples_hint = f" (quantity: {test_triples_size})"
         elif generation_level == 2:
             output_triples = '\n'.join([f'  - {h} -> {r} -> (predict entity here)' for (h, r, t) in test_triples])
+            output_triples_hint = f" (quantity: {test_triples_size})"
         elif generation_level == 3:
             output_triples = '\n'.join([f'  - {h} -> (predict relation here) -> (predict entity here)' for (h, r, t) in test_triples])
+            output_triples_hint = f" (quantity: {test_triples_size})"
         elif generation_level == 4:
             output_triples = "(predict triples here)"
+            output_triples_hint = f" (quantity: {test_triples_size})"
+        elif generation_level == 5:
+            output_triples = "(predict triples here)"
+            output_triples_hint = ""
         else:
             assert False, f"Invalid generation_level: {generation_level}"
         generation_prompt = generation_prompt.format(
             test_entity=test_entity,
-            test_triples_size=test_triples_size,
             output_triples=output_triples,
+            output_triples_hint=output_triples_hint,
         )
-        # print(f"generation_prompt2={generation_prompt}")
-        # exit(1)
-        chat_history.append({"role": "user", "content": generation_prompt})
 
+        chat_history.append({"role": "user", "content": generation_prompt})
         tot_words = []
         tot_chars = []
         tot_seconds = []
@@ -147,6 +155,7 @@ with JobTimer("KG Generation", rt=1, rb=1, rw=114, rc='=', mt=1, verbose=1):
                 num_words = len(model_output.split())
                 item["responses"].append({
                     "model": target_model,
+                    "generation_level": generation_level,
                     "output": model_output,
                     "words": num_words,
                     "chars": num_chars,
@@ -162,15 +171,5 @@ with JobTimer("KG Generation", rt=1, rb=1, rw=114, rc='=', mt=1, verbose=1):
                 item["no_responses"].append(target_model)
             save_json(total_data, output_file, indent=2, ensure_ascii=False)
 
-            print("\n" * 3)
-            print(f"model = \n{target_model}")
-            print("\n" * 3)
-            print(f"model_output = \n{model_output}")
-            print("\n" * 3)
-            aaa = '\n'.join([f'  - {h} -> {r} -> {t}' for (h, r, t) in test_triples])
-            print(f"test_triples = \n{aaa}")
-            print("\n" * 3)
-            print("=" * 120)
-            print("\n" * 3)
-
-print(total_data)
+# write to output file (final save)
+save_json(total_data, output_file, indent=2, ensure_ascii=False)
