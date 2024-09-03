7aad4ae4a3eea6af1b5e6bfdcd349ffe43c38f36
Jihee Ryu <chrisjihee@etri.re.kr>
2024-09-03 18:43:34 +0900

    Testing with GPT-4 and LLaMA-3.1

--- extraction-by-GPT.py
+++ extraction-by-GPT.py
@@ -18,30 +18,46 @@ args = CommonArguments(
         msg_format=LoggingFormat.PRINT_00,
     )
 )
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = read_or("conf/key-openai.txt") or getpass("OpenAI API key: ")
+if "TOGETHER_API_KEY" not in os.environ:
+    os.environ["TOGETHER_API_KEY"] = read_or("conf/key-togetherai.txt") or getpass("TogetherAI API key: ")
 
 # setup program
 test_size = 100
 debug_test_size = 1
 
-request_timeout = 300
-default_max_tokens = 512
-default_temperature = 0.5
-default_repetition_penalty = 1.1
-api_client = OpenAI(timeout=request_timeout,
-                    api_key=read_or("conf/key-openai.txt") or getpass("OpenAI API key: "))
+openai_assistants = [
+    "gpt-4o-2024-08-06",
+    # "gpt-4o-mini"
+]
+togethers_assistants = [
+    "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
+    # "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo"
+]
+openai_client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+together_client = Together(api_key=os.environ.get('TOGETHER_API_KEY'))
 
 
-# define function to chat with GPT
-def chat_with_gpt(messages, model_id="gpt-4o"):
+# define function to chat with LLM through OpenAI
+def chat_with_llm_by_OpenAI(**kwargs):
     try:
-        stream = api_client.chat.completions.create(
-            model=model_id,
-            messages=messages,
-            stream=True,
-        )
-        output = ''.join(chunk.choices[0].delta.content or '' for chunk in stream)
-        return output.strip()
-    except:
+        response = openai_client.chat.completions.create(**kwargs)
+        output = response.choices[0].message.content
+        return output
+    except Exception as e:
+        logger.error("Exception:", e)
+        return None
+
+
+# define function to chat with LLM through TogetherAI
+def chat_with_llm_by_Together(**kwargs):
+    try:
+        response = together_client.chat.completions.create(**kwargs)
+        output = response.choices[0].message.content
+        return output
+    except Exception as e:
+        logger.error("Exception:", e)
         return None
 
 
@@ -69,8 +85,6 @@ for generation_level in target_generation_levels:
                   rt=1, rb=1, rw=114, rc='=', mt=1, verbose=1):
         output_data = []
         for i, item in enumerate(test_data, start=1):
-            output_data.append(item)
-            chat_history = []
             entity = item["entity"]
             triples = item["triples"]
             messages = item["messages"]
@@ -79,11 +93,37 @@ for generation_level in target_generation_levels:
                 assert response["level"] == generation_level, f"level={response['level']} != generation_level={generation_level}"
                 model = response["model"].split("/")[-1]
                 output = response["output"]
-                model_responses.append(f"\n<BEGIN_OF_MODEL_RESPONSE ({j}. {model})>\n{output}\n<END_OF_MODEL_RESPONSE>\n\n")
+                model_responses.append(f"\n<BEGIN_OF_MODEL_RESPONSE ({j}. {model})>\n{output}\n<END_OF_MODEL_RESPONSE ({j}. {model})>\n\n")
             custom_prompt = common_prompt.format(
                 entity=entity,
                 triples_by_human='\n'.join([f'  - {h} -> {r} -> {t}' for (h, r, t) in triples]),
                 model_responses="\n".join(model_responses),
                 num_model=len(model_responses),
             )
+            chat_history = [
+                {
+                    "role": "system",
+                    "content": "You will be provided with unstructured data, and your task is to parse it into JSON format."
+                },
+                {
+                    "role": "user",
+                    "content": custom_prompt
+                }
+            ]
+            output_data.append({
+                "entity": entity,
+                "triples": triples,
+            })
             print(custom_prompt)
+            for teacher in openai_assistants:
+                model_output = chat_with_llm_by_OpenAI(messages=chat_history, model=teacher, max_tokens=2048)
+                print(f"=== GPT [{teacher}] ===")
+                print(model_output)
+                print("=" * 80)
+                print("\n\n")
+            for teacher in togethers_assistants:
+                model_output = chat_with_llm_by_Together(messages=chat_history, model=teacher, max_tokens=2048)
+                print(f"=== LLaMA [{teacher}] ===")
+                print(model_output)
+                print("=" * 80)
+                print("\n\n")
