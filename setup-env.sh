73075900b1e27d60bcff5beb70d2f331ecfa281b
Jihee Ryu <chrisjihee@etri.re.kr>
2024-09-03 22:03:02 +0900

    Update requirements.txt, setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -5,11 +5,11 @@ bash Miniforge3-$(uname)-$(uname -m).sh
 
 # basic
 mamba create -n LLM-based python=3.11 -y; mamba activate LLM-based
-pip install -r requirements.txt
+pip install -U -r requirements.txt
 
 # chrisbase
 rm -rf chrisbase*; git clone git@github.com:chrisjihee/chrisbase.git
-pip install --editable chrisbase*
+pip install -U -e chrisbase*
 
 # list
-pip list | grep -E "langchain|transformers|torch|faiss|groq|replicate|together|chris"
+pip list | grep -E "langchain|transformers|torch|faiss|groq|replicate|together|openai|chris"
