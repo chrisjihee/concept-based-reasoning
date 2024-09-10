00d53fefd61f85b0044fada2c136e4cab5103ca4
Jihee Ryu <chrisjihee@etri.re.kr>
2024-09-10 13:24:34 +0900

    Rename to evaluation-KG-gen.py, evaluation-KG-ext.py

--- evaluation-KG-gen.py
+++ evaluation-KG-gen.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 args = CommonArguments(
     env=ProjectEnv(
         project="LLM-based",
-        job_name="evaluation-by-extraction",
+        job_name="evaluation-KG-gen",
         msg_level=logging.INFO,
         msg_format=LoggingFormat.BRIEF_00,
     )
@@ -66,21 +66,21 @@ BASIC_EXCEPTIONS = [JSON_RANGE_ERROR, JSON_FORMAT_ERROR, JSON_KEY_ERROR]
 # run program
 for dataset_name in dataset_names:
     for generation_level in target_generation_levels:
-        extraction_file = f"extraction/{dataset_name}/edges_as_text_all-responses-{test_size}@{generation_level}.json"
+        generation_file = f"generation/{dataset_name}/edges_as_text_all-responses-{test_size}@{generation_level}.json"
         evaluation_file = f"evaluation/{dataset_name}/{args.env.job_name}-{test_size}@{generation_level}.xlsx"
-        extraction_data = load_json(extraction_file)
+        generation_data = load_json(generation_file)
         if debug_test_size > 0:
-            extraction_data = extraction_data[:debug_test_size]
+            generation_data = generation_data[:debug_test_size]
 
-        with JobTimer(f"LLM Evaluation(dataset_name={dataset_name}, generation_level={generation_level}, num_extraction={len(extraction_data)})",
+        with JobTimer(f"LLM Evaluation(dataset_name={dataset_name}, generation_level={generation_level}, num_generation={len(generation_data)})",
                       rt=1, rb=1, rw=114, rc='=', mt=1, verbose=1):
 
             evaluation_data = []
-            for i, sample in enumerate(tqdm(extraction_data, desc=f"* Evaluating LLM", unit="sample", file=sys.stdout), start=1):
+            for i, sample in enumerate(tqdm(generation_data, desc=f"* Evaluating LLM", unit="sample", file=sys.stdout), start=1):
                 entity = sample["entity"]
-                triples_by_human = normalize_triples(sample["triples"])
-                extraction_messages = sample["extraction_messages"]
-                extraction_outputs = sample["responses"]
+                triples_by_human = normalize_triples(sample["triples_by_human"])
+                generation_messages = sample["generation_messages"]
+                generation_outputs = sample["generation_outputs"]
 
                 # DEBUG PRINT
                 # print()
@@ -91,86 +91,84 @@ for dataset_name in dataset_names:
                 # for x in triples_by_human:
                 #     print("  -", x)
                 # print("-" * 100)
-                # print(f"extraction_messages: \n{extraction_messages[-1]['content']}")
+                # print(f"generation_messages: \n{generation_messages[-1]['content']}")
                 # print("-" * 100)
 
-                for j, extraction_output in enumerate(extraction_outputs, start=1):
-                    extraction_model = extraction_output["model"].split("/")[-1]
-                    content = str(extraction_output["output"]["content"])
+                for j, generation_output in enumerate(generation_outputs, start=1):
+                    generation_type = generation_output["type"]
+                    generation_model = generation_output["model"].split("/")[-1]
+                    content = str(generation_output["output"]["content"])
                     # print(f"content: {content}")
-                    finish_reason = extraction_output["output"]["finish_reason"]
-                    narrator_role = extraction_output['output']['role'].upper()
+                    finish_reason = generation_output["output"]["finish_reason"]
+                    narrator_role = generation_output['output']['role'].upper()
                     if narrator_role in successful_narrator_roles:
                         if finish_reason in successful_finish_reasons:
-                            if '[' in content and ']' in content and content.index('[') < content.rindex(']'):
+                            if '{' in content and '}' in content and content.index('{') < content.rindex('}'):
                                 try:
-                                    predictions = json.loads(content[content.index('['):content.rindex(']') + 1])
-                                    # print(f"predictions: {predictions}")
-                                    for prediction in predictions:
-                                        generation_model = prediction["model_id"] if "model_id" in prediction else "unknown"
-                                        if "triples_by_model" in prediction and isinstance(prediction["triples_by_model"], (list, tuple)):
-                                            triples_by_model = normalize_triples(prediction["triples_by_model"])
-                                            prec, rec, f1 = measure_performance(triples_by_human, triples_by_model)
-                                            evaluation_data.append({
-                                                "i": i,
-                                                "j": j,
-                                                "type": "generation",
-                                                "model": generation_model,
-                                                "prec": prec,
-                                                "rec": rec,
-                                                "f1": f1,
-                                                "exception": np.nan,
-                                            })
-                                        else:
-                                            evaluation_data.append({
-                                                "i": i,
-                                                "j": j,
-                                                "type": "generation",
-                                                "model": generation_model,
-                                                "exception": JSON_KEY_ERROR,
-                                            })
+                                    prediction = json.loads(content[content.index('{'):content.rindex('}') + 1])
+                                    # print(f"prediction: {prediction}")
+                                    if "triples_by_model" in prediction and isinstance(prediction["triples_by_model"], (list, tuple)):
+                                        triples_by_model = normalize_triples(prediction["triples_by_model"])
+                                        prec, rec, f1 = measure_performance(triples_by_human, triples_by_model)
+                                        evaluation_data.append({
+                                            "i": i,
+                                            "j": j,
+                                            "type": generation_type,
+                                            "model": generation_model,
+                                            "prec": prec,
+                                            "rec": rec,
+                                            "f1": f1
+                                        })
+                                    else:
+                                        evaluation_data.append({
+                                            "i": i,
+                                            "j": j,
+                                            "type": generation_type,
+                                            "model": generation_model,
+                                            "exception": JSON_KEY_ERROR,
+                                        })
                                 except JSONDecodeError:
                                     evaluation_data.append({
                                         "i": i,
                                         "j": j,
-                                        "type": "extraction",
-                                        "model": extraction_model,
+                                        "type": generation_type,
+                                        "model": generation_model,
                                         "exception": JSON_FORMAT_ERROR,
                                     })
                             else:
                                 evaluation_data.append({
                                     "i": i,
                                     "j": j,
-                                    "type": "extraction",
-                                    "model": extraction_model,
+                                    "type": generation_type,
+                                    "model": generation_model,
                                     "exception": JSON_RANGE_ERROR
                                 })
                         else:
                             evaluation_data.append({
                                 "i": i,
                                 "j": j,
-                                "type": "extraction",
-                                "model": extraction_model,
+                                "type": generation_type,
+                                "model": generation_model,
                                 "exception": f"{narrator_role}: {finish_reason}"
                             })
                     else:
                         evaluation_data.append({
                             "i": i,
                             "j": j,
-                            "type": "extraction",
-                            "model": extraction_model,
+                            "type": generation_type,
+                            "model": generation_model,
                             "exception": f"{narrator_role}: {finish_reason}"
                         })
 
             evaluation_data = pd.DataFrame(evaluation_data)
-            evaluation_summary = evaluation_data.groupby(['type', 'model']).agg(
+            evaluation_summary = evaluation_data.groupby(['model', 'type']).agg(
                 prec_mean=('prec', 'mean'),
                 rec_mean=('rec', 'mean'),
                 f1_mean=('f1', 'mean'),
                 valid_count=('f1', lambda x: x.notna().sum()),
                 invalid_count=('exception', 'count'),
                 exception_counts=('exception', lambda x: x.value_counts().to_dict())
-            ).reset_index().sort_values(by=['type', 'model'])
+            ).reset_index().sort_values(by=['model', 'type'])
 
             exception_counts = evaluation_summary['exception_counts'].apply(pd.Series).fillna(0).astype(int)
             exception_counts = exception_counts.reindex(
